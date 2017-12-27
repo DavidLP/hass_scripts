47e2e59ab79e97b91041126329669e146798281e
DavidLP <david-leon.pohl@rub.de>
2017-12-27 14:48:32 +0100

    Change project name

--- hass_scripts/__init__.py
+++ hass_scripts/__init__.py
@@ -1,7 +1,7 @@
 # http://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
 from pkg_resources import get_distribution, DistributionNotFound
 
-__project__ = 'mqtt_tts'
+__project__ = 'hass_scripts'
 __version__ = None  # required for initial installation
 
 try:
