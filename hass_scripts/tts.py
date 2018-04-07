b1a1df1c4f33860f4baadee8a92db7e9e1263935
DavidLP <david-leon.pohl@rub.de>
2018-04-07 09:35:35 +0200

    ENH: add monitor turn on feature

--- hass_scripts/tts.py
+++ hass_scripts/tts.py
@@ -1,8 +1,6 @@
 import logging
 import asyncio
-import os
 import tempfile
-
 from hbmqtt import client
 from hbmqtt.mqtt.constants import QOS_0
 from gtts import gTTS
@@ -14,27 +12,90 @@ ALIVE_TIME = 300  # in seconds
 # Used console command to play mp3 file
 MP3_CMD = 'mpg123 -q'
 # MQTT settings
-MQTT_SERVER = 'mqtts://192.168.1.200:8883'
-MQTT_TOPIC = '/devices/tts/#'
-MQTT_CA = '/home/pi/mosquitto/ca.crt'
-MQTT_CRT = '/home/pi/mosquitto/rpi2.crt'
-MQTT_KEY = '/home/pi/mosquitto/rpi2.key'
+MQTT_SERVER = 'mqtts://pfuideibel.dnshome.de:8883'
+MQTT_TOPIC = 'devices/#'
+MQTT_CA = '/home/ha/mosquitto_certs/ca.crt'
+MQTT_CRT = '/home/ha/mosquitto_certs/rpi2.crt'
+MQTT_KEY = '/home/ha/mosquitto_certs/rpi2.key'
 
 
-def output_google(text, lang):
+async def run_command_shell(command):
+    """Run command in subprocess (shell)
+    
+    Note:
+        This can be used if you wish to execute e.g. "copy"
+        on Windows, which can only be executed in the shell.
+    """
+    # Create subprocess
+    process = await asyncio.create_subprocess_shell(
+        command,
+        stdout=asyncio.subprocess.PIPE)
+
+    # Status
+    # print('Started:', command, '(pid = ' + str(process.pid) + ')')
+
+    # Wait for the subprocess to finish
+    stdout, stderr = await process.communicate()
+
+    # Progress
+    #if process.returncode == 0:
+    #    print('Done:', command, '(pid = ' + str(process.pid) + ')')
+    #else:
+    #    print('Failed:', command, '(pid = ' + str(process.pid) + ')')
+
+    # Result
+    result = stdout.decode().strip()
+
+    # Return stdout
+    return result
+
+
+async def output_google(text, lang):
     tts = gTTS(text=text, lang=lang)
-    f = tempfile.NamedTemporaryFile(suffix='.mp3')
+    f = tempfile.NamedTemporaryFile(suffix='.mp3', delete=True)
     tts.save(f.name)
-    os.system(MP3_CMD + ' ' + f.name)
+    await asyncio.sleep(3)
+    logging.info('Google TTS (%s): %s', lang, text)
+    await run_command_shell(MP3_CMD + ' ' + f.name)
     f.close()
 
 
-def output(text, lang):
+async def output(text, lang):
     logger.info('Speak %s', text)
+    if not 'On' in await monitor_status():
+      await run_command_shell('export DISPLAY=:0; xset dpms force on')
+      await asyncio.sleep(5)
     try:
-        output_google(text, lang)
-    except:  # Fallback if no internet connection
-        os.system('espeak' + ' "' + text + '"')
+        await output_google(text, lang)
+    except IOError:  # Fallback if no internet connection
+        await run_command_shell('espeak' + ' "' + text + '"')
+
+async def test_pub():
+    while True:
+        logging.info('TESZ PUB')
+        asyncio.ensure_future(c.publish('a/b', b'TEST MESSAGE WITH QOS_0'))
+        await asyncio.sleep(2)
+
+async def monitor_status():
+    r = await run_command_shell('export DISPLAY=:0; xset q')
+    return str(r[-8:])
+
+async def check_monitor_status():
+    status = None
+    while True:
+        stat_s = await monitor_status()
+        logging.info(stat_s)
+        if 'On' in stat_s:
+            logging.info('MONITOR IS ON')
+            if not status:
+                asyncio.ensure_future(c.publish('devices/monitor/status', b'ON'))
+                status = True
+        elif 'Off' in stat_s:
+            logging.info('MONITOR IS OFF')
+            if status or status is None:
+                asyncio.ensure_future(c.publish('devices/monitor/status', b'OFF'))
+                status = False 
+        await asyncio.sleep(2)  # Poll every 2 seconds
 
 async def keep_alive():
     ''' Send empty sounds to prevent speaker power down '''
@@ -49,11 +110,17 @@ async def consumer():
     while True:
         topic, text = await q.get()
         text = text.decode('ascii')
-        if 'say' in topic:
-            output(text, lang='en')
-        elif 'sag' in topic:
-            output(text, lang='de')
-
+        if 'tts' in topic:
+            if 'say' in topic:
+                await output(text, lang='en')
+            elif 'sag' in topic:
+                await output(text, lang='de')
+        if 'monitor' in topic:
+            if 'status' not in topic:
+                if 'ON' in text:
+                    await run_command_shell('export DISPLAY=:0; xset dpms force on')
+                else:
+                    await run_command_shell('export DISPLAY=:0; xset dpms force off')
 
 async def mqtt_rcv():
     await c.connect(MQTT_SERVER, cafile=MQTT_CA)
@@ -86,8 +153,10 @@ async def main():
     global q
     q = asyncio.Queue()
     loop = asyncio.get_event_loop()
-    loop.create_task(keep_alive())
+    # loop.create_task(keep_alive())
     loop.create_task(consumer())
+    loop.create_task(check_monitor_status())
+    # loop.create_task(test_pub())
     loop.create_task(mqtt_rcv())
 
 if __name__ == '__main__':
